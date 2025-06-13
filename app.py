8a73f4061be8114f6246664512887ce059ba131c
Isaac Johnson <isaac.johnson@gmail.com>
2025-06-13 08:40:14 -0500

    simplify the OAuth registration to handle missing jwks_uri on OAuth redirect. Update chart with new min Memory settings from Goldilocks

--- app.py
+++ app.py
@@ -36,12 +36,7 @@ oauth.register(
     name='google',
     client_id=GOOGLE_CLIENT_ID,
     client_secret=GOOGLE_CLIENT_SECRET,
-    access_token_url='https://oauth2.googleapis.com/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'},
 )
 
